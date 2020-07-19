4074936b5b18935d737fd4ebda743c5316e5fd92
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-19 15:26:22 +0100

    Func to class component & two methods
    - Changed from func to class component.
    - Wrote a new method that grabs the user's current location.
    - Wrote a new method that requests permission to use the device's location services.

--- App.js
+++ App.js
@@ -1,31 +1,74 @@
 import React from 'react';
 import MapView, { PROVIDER_GOOGLE,  Marker } from 'react-native-maps';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, Platform } from 'react-native';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
 
-const App = () => {
+class App extends React.Component {
 
-  return (
-    <View style={styles.container}>
-      {/* Expands the map to fit the entire viewport */}
-      <MapView 
-        style={styles.mapStyle}
-        showsUserLocation={true}
-        followsUserLocation={true}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}>
-        <Marker
-          coordinate={{ latitude: 37.7825259, longitude: -122.4351431 }}
-          title={"San Francisco"}>
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0.0,
+      longitude: 0.0,
+    }
+  }
+  
+  // Gets the current location of the user
+  locateCurrentPosition = () => {
+    Geolocation.getCurrentPosition(position => {
+      console.log(JSON.stringify(position));
+    });
+  }
 
-        </Marker>
-      </MapView>
-    </View>
-  );
+  // Request permission to use device's location services
+  requestLocationPermission = async () => {
+  
+    if (Platform.OS === "android") {
+      // Requesting permission to access location service (Android)
+      let response = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      // Capture the user's current location after permission has been granted
+      if (response === "granted") {
+        this.locateCurrentPosition();
+      }
+    } else {
+      // Requesting permission to access location service (iOS)
+      let response = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      // Capture the user's current location after permission has been granted
+      if (response === "granted") {
+        this.locateCurrentPosition();
+      }
+    }
+  }
+
+
+  componentDidMount() {
+    this.requestLocationPermission();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* Expands the map to fit the entire viewport */}
+        <MapView 
+          style={styles.mapStyle}
+          showsUserLocation={true}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}>
+          <Marker
+            coordinate={{ latitude: 37.7825259, longitude: -122.4351431 }}
+            title={"San Francisco"}>
+  
+          </Marker>
+        </MapView>
+      </View>
+    );
+  }
   
 }
 
