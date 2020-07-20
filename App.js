149bbd47803387c8e6975e47c32fd1b5245b4d0a
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-20 14:08:03 +0100

    Method that fetches places and installed packages

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MapView, { PROVIDER_GOOGLE,  Marker } from 'react-native-maps';
 import { StyleSheet, View, Dimensions, Alert } from 'react-native';
+import Axios from 'axios';
+import { API_KEY } from "@env";
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      places: []
+    }
   }
   
   // Find the current location of the user
@@ -27,6 +31,23 @@ class App extends React.Component {
     { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
   }
+
+  // Fetches places based on timestamp & user location 
+  getPlaces = () => {
+    const apiEndpoint = "https://crowdless.com/default/places";
+    
+    let currentDate = new Date();
+    let timestamp = currentDate.toUTCString(); 
+
+    console.log(`${apiEndpoint}\n${API_KEY}\n${timestamp}`);
+
+    // const body = {
+    //   coords: { lat: location.latitude, lng: location.longitude },
+    //   time_stamp: ""
+    // }
+  }
+
+  showCallout = (place) => console.log(`Show callout called ${place}`);
 
   componentDidMount() {
     this.findCurrentLocation();
