83843dda40e90baa019dcb8457921895c3c691f9
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-21 21:50:59 +0100

    Place component is complete

--- components/Place.jsx
+++ components/Place.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Image } from 'reac-native';
+import { Image } from 'react-native';
 import { Marker } from 'react-native-maps';
 
 // This functional component represents each location as a busy-level image
-const Place = ({ latitude, longitude, busyLvl }) => {
+const Place = ({ key, latitude, longitude, busyLvl }) => {
     let busy = busyLvl.toLowerCase();
 
+    // Displays location name in console
+    const showCallout = (place) => {
+        console.log(`Show callout called ${place}`);
+    }
+
     return (
-        <Marker coordinate={ latitude, longitude }>
+        <Marker coordinate={ latitude, longitude } onPress={showCallout(key)}>
             <Image source={require(`../assets/${busy}.png`)} />
         </Marker>
     );
